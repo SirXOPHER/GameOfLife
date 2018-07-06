cb9705f9e3badd475776fa7e6c978244c09e0bbc
SirXOPHER <chris.reinmuller@gmail.com>
2018-07-06 11:18:03 +0100

    Add failing test 'ensureEmptyUniverseIsStillEmptyAfterFirstGeneration()'

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -3,4 +3,8 @@ public class Game {
     public boolean isEmpty() {
         return true;
     }
+
+    public Game evolve() {
+        return null;
+    }
 }
--- src/test/java/GameOfLifeShould.java
+++ src/test/java/GameOfLifeShould.java
@@ -9,4 +9,9 @@ public class GameOfLifeShould {
     public void haveEmptyUniverseWithNoSeed() {
         assertThat(new Game().isEmpty(), is(true));
     }
+
+    @Test
+    public void ensureEmptyUniverseIsStillEmptyAfterFirstGeneration() {
+        assertThat(new Game().evolve().isEmpty(), is(true));
+    }
 }
