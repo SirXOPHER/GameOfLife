8cd8ba6c9c49eac4e8324797780b226fec2bbf06
SirXOPHER <chris.reinmuller@gmail.com>
2018-07-06 14:01:19 +0100

    Refactor test 'preserveACellWithThreeNeighbours()'

--- src/test/java/GameOfLifeShould.java
+++ src/test/java/GameOfLifeShould.java
@@ -1,9 +1,13 @@
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 
+@RunWith(JUnitParamsRunner.class)
 public class GameOfLifeShould {
 
     @Test
@@ -33,31 +37,27 @@ public class GameOfLifeShould {
     }
 
     @Test
-    public void killACellWithOneNeighbour() {
-        Game game = new Game(new Cell(0, 0), new Cell(0, 1));
+    @Parameters(method = "getGameBeforeAndAfterEvolution")
+    public void evolveGame(Game gameBeforeEvolution, Game expectedGameAfterEvolution) {
 
-        Game nextGameGeneration = game.evolve();
+        Game afterEvolutionGame = gameBeforeEvolution.evolve();
 
-        assertThat(nextGameGeneration.isEmpty(), is(true));
+        assertThat(afterEvolutionGame, is(equalTo(expectedGameAfterEvolution)));
     }
 
-    @Test
-    public void preserveACellWithTwoNeighbours() {
-        Game game = new Game(new Cell(0,0), new Cell(0,1), new Cell(1,0));
-        Game expectedGame = new Game(new Cell(0, 0), new Cell(0, 1), new Cell(1, 0));
-
-        Game nextGameGeneration = game.evolve();
-
-        assertThat(nextGameGeneration.evolve(), is(equalTo(expectedGame)));
-    }
-
-    @Test
-    public void preserveACellWithThreeNeighbours() {
-        Game game = new Game(new Cell(0,0), new Cell(0,1), new Cell(1,0), new Cell(1, 1));
-        Game expectedGame = new Game(new Cell(0, 0), new Cell(0, 1), new Cell(1, 0), new Cell(1, 1));
-
-        Game nextGameGeneration = game.evolve();
-
-        assertThat(nextGameGeneration.evolve(), is(equalTo(expectedGame)));
+    public Object[][] getGameBeforeAndAfterEvolution(){
+        return new Object[][]{
+                new Object[]{
+                        new Game(new Cell(0,0)), new Game()
+                },
+                new Object[]{
+                        new Game(new Cell(0,0), new Cell(0,1), new Cell(1,0)),
+                        new Game(new Cell(0, 0), new Cell(0, 1), new Cell(1, 0))
+                },
+                new Object[]{
+                        new Game(new Cell(0,0), new Cell(0,1), new Cell(1,0), new Cell(1, 1)),
+                        new Game(new Cell(0, 0), new Cell(0, 1), new Cell(1, 0), new Cell(1, 1))
+                }
+        };
     }
 }
