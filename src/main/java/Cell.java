b3bcd87f6ae2cce1b1c85b86402c3c467a60571a
SirXOPHER <chris.reinmuller@gmail.com>
2018-07-06 13:46:24 +0100

    Pass test 'preserveACellWithTwoNeighbor()'

--- src/main/java/Cell.java
+++ src/main/java/Cell.java
@@ -1,6 +1,41 @@
 
 class Cell {
 
-    public Cell(int x, int y) {
+    private final int x;
+    private final int y;
+
+    Cell(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cell cell = (Cell) o;
+
+        return x == cell.x && y == cell.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Cell{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    boolean isNeighbor(Cell cell) {
+        return Math.abs(cell.x - x) <= 1
+                && Math.abs(cell.y - y) <= 1;
     }
 }
--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,20 +1,55 @@
-import java.util.Arrays;
-import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 class Game {
 
-    private final List<Cell> cells;
+    private final Set<Cell> cells;
 
-    Game(Cell... cell){
-        this.cells = Arrays.asList(cell);
+    Game(Cell... cell) {
+        this.cells = Stream.of(cell).collect(Collectors.toSet());
+    }
+
+    private Game(Set<Cell> cells) {
+        this.cells = cells;
+    }˚
+
+    Game evolve() {
+        Set<Cell> survivingCells = cells.stream()
+                .filter(this::survive)
+                .collect(Collectors.toSet());
+        return new Game(survivingCells);
     }
 
     boolean isEmpty() {
         return cells.isEmpty();
     }
 
-    Game evolve() {
-        return new Game();
+    private boolean survive(Cell cell) {
+        return cells.stream().filter(c -> !c.equals(cell))
+                .filter(cell::isNeighbor)
+                .count() >= 2;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Game game = (Game) o;
+
+        return cells != null ? cells.equals(game.cells) : game.cells == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return cells != null ? cells.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" +
+                "cells=" + cells +
+                '}';
+    }
 }
