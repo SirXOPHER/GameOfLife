dc56b30728fa6b0a9c9d9a996fc005e3ba34e2fd
SirXOPHER <chris.reinmuller@gmail.com>
2018-07-06 11:48:41 +0100

    Add failing test 'killACellWithNoNeighbor()'

--- src/test/java/GameOfLifeShould.java
+++ src/test/java/GameOfLifeShould.java
@@ -20,4 +20,11 @@ public class GameOfLifeShould {
         Game game = new Game(new Cell());
         assertThat(game.isEmpty(), is(false));
     }
+
+    @Test
+    public void killACellWithNoNeighbor() {
+        Game game = new Game(new Cell());
+        assertThat(game.evolve().isEmpty(), is (true));
+
+    }
 }
